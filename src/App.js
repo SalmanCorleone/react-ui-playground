5444085ae2d2ab38c139956142bcc5585ca624a1
salman <samilsalman12@gmail.com>
2020-08-17 13:02:23 +0600

    Make working slider anim

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import HorizontalScroll from './components/HorizontalScroll';
+import Axios from 'axios';
 
 function App() {
+  const [data, setData] = useState();
+
+  useEffect(() => {
+    const getFakeData = async () => {
+      try {
+        let res = await Axios.get('https://jsonplaceholder.typicode.com/todos');
+        if (res.status) {
+          setData(res.data);
+        }
+        console.log(res);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    getFakeData();
+  }, []);
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <HorizontalScroll />
+        <HorizontalScroll {...{ data }} />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
--- src/components/HorizontalScroll.js
+++ src/components/HorizontalScroll.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../css/HorizontalScroll.css';
 
-function HorizontalScroll() {
+function HorizontalScroll({ data }) {
+  const [activeIndex, setActiveIndex] = useState(40);
+  const [anim, setAnim] = useState('');
+
+  useEffect(() => {}, [anim]);
+
+  const next = () => {
+    setAnim('fromLeft');
+    setActiveIndex(activeIndex + 1);
+  };
+
+  const prev = () => {
+    setAnim('fromRight');
+    setActiveIndex(activeIndex - 1);
+  };
+
+  const clear = () => setAnim('');
+
   return (
     <div className="container">
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
-      <p>Hey World</p>
+      {/* content */}
+      <div className="box" />
+      {data && (
+        <div className="item" onAnimationEnd={clear} anim={anim}>
+          <p>{JSON.stringify(data[activeIndex])}</p>
+          <p>{activeIndex}</p>
+        </div>
+      )}
+
+      {/* controller */}
+      <div className="controller">
+        <button onClick={prev}>Prev</button>
+        <button onClick={next}>Next</button>
+      </div>
     </div>
   );
 }
