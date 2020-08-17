3cff64956637d753970cf7fdb54fcbb8fba129b7
salman <samilsalman12@gmail.com>
2020-08-17 11:26:28 +0600

    Init Project

---
+++ src/components/HorizontalScroll.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import '../css/HorizontalScroll.css';
+
+function HorizontalScroll() {
+  return (
+    <div className="container">
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+      <p>Hey World</p>
+    </div>
+  );
+}
+
+export default HorizontalScroll;
