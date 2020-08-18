c36c42afe89c3332152abe222b7bb08a7f644ee6
salman <samilsalman12@gmail.com>
2020-08-18 12:18:21 +0600

    Add more motions

--- src/components/animated/Animated.js
+++ src/components/animated/Animated.js
@@ -3,15 +3,43 @@ import { motion } from 'framer-motion';
 import './Animated.css';
 
 const Animated = () => {
+  const list = {
+    visible: {
+      opacity: 1,
+      transition: {
+        when: 'beforeChildren',
+        staggerChildren: 0.1,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      transition: {
+        when: 'afterChildren',
+      },
+    },
+  };
+
+  const item = {
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: -100 },
+  };
+
   return (
-    <motion.div
-      animate={{ x: -100 }}
-      transition={{ duration: 0.2 }}
-      className="container">
-      <p>Hello Anim</p>
-      <p>Hello Anim</p>
-      <p>Hello Anim</p>
-      <p>Hello Anim</p>
+    <motion.div className="container">
+      <motion.ul initial="hidden" animate="visible" variants={list}>
+        <motion.li variants={item}>
+          <p>dsadasdasd</p>
+        </motion.li>
+        <motion.li variants={item}>
+          <p>dsadasdasd</p>
+        </motion.li>
+        <motion.li variants={item}>
+          <p>dsadasdasd</p>
+        </motion.li>
+        <motion.li variants={item}>
+          <p>dsadasdasd</p>
+        </motion.li>
+      </motion.ul>
     </motion.div>
   );
 };
